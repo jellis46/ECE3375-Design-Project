d4149d2e96462d3ebcfd237769f8c4d0ac563e30
jellis46 <35785041+jellis46@users.noreply.github.com>
2019-04-08 18:28:27 -0400

    finished project

--- designproject.c
+++ designproject.c
@@ -20,11 +20,11 @@ int main(void) {
     int time_hr = 0;
     int time_10hr = 0;
 
-    int alarm_time_s = 0;
-    int alarm_time_10s = 0;
+    int alarm_time_s = 5;
+    int alarm_time_10s = 1;
     int alarm_time_min = 0;
     int alarm_time_10min = 0;
-    int alarm_time_hr = 7;
+    int alarm_time_hr = 0;
     int alarm_time_10hr = 0;
 
     //displays
@@ -34,6 +34,13 @@ int main(void) {
     volatile char * display_10min = (char *)(HEX3_HEX0_BASE + 3);
     volatile char * display_hr = (char *)HEX5_HEX4_BASE;
     volatile char * display_10hr = (char *)(HEX5_HEX4_BASE + 1);
+	
+    DisplayTime(time_s, display_s);
+    DisplayTime(time_10s, display_10s);
+    DisplayTime(time_min, display_min);
+    DisplayTime(time_10min, display_10min);
+    DisplayTime(time_hr, display_hr);
+    DisplayTime(time_10hr, display_10hr);
     
     volatile int * LED_ptr = (int *)LED_BASE;
 
@@ -43,6 +50,7 @@ int main(void) {
 	int ledStatus = 0;
 	int ledCounter = 0;
     int alarmShutoffThreshold = 50;
+	int buttonPressed = 0;
     
     volatile int * key_ptr = (int *)KEY_BASE;
 	int temp;
@@ -67,7 +75,7 @@ int main(void) {
                 time_10min = 0;
                 time_hr++;
             }
-            if (time_hr == 10) {
+            if (time_hr == 10 || (time_hr == 4 && time_10hr == 2)) {
                 time_hr = 0;
                 time_10hr++;
             }
@@ -94,16 +102,19 @@ int main(void) {
 
         if (alarmActive == 1) {//flash LED when alarm is active
 			ledCounter++;
-			if (ledCounter == 500000) {
+			if (ledCounter == 50000) {
 				if (ledStatus == 0)
 				{
                     ledStatus = 1;
+					ledCounter = 0;
                     *(LED_ptr) |= 0x1;
 				}
 				else
 				{
                     ledStatus = 0;
+					ledCounter = 0;
                     *(LED_ptr) &= ~0x1;
+					
 				}
 			}
 
@@ -113,8 +124,9 @@ int main(void) {
             }
         }
 
-        if (*(key_ptr) == 1) {//toggle clock mode
+        if (*(key_ptr) == 1 && buttonPressed == 0) {//toggle clock mode
 
+			buttonPressed = 1;
             if (clockMode == 0) {
                 clockMode = 1;
                 alarmSetSegment = 1;
@@ -134,15 +146,20 @@ int main(void) {
                 DisplayTime(time_10hr, display_10hr);
             }
                 
-        } else if (*(key_ptr) == 2) {//change what time segment is being adjusted
+        } else if (*(key_ptr) == 2 && buttonPressed == 0) {//change what time segment is being adjusted
 
-            alarmSetSegment++;
-            if (alarmSetSegment = 4) {
-                alarmSetSegment = 1;
-            }
+			buttonPressed = 1;
+			if (alarmSetSegment == 1) {
+				alarmSetSegment = 2;
+			} else if (alarmSetSegment == 2) {
+				alarmSetSegment = 3;
+			} else if (alarmSetSegment == 3) {
+				alarmSetSegment = 1;
+			}
 
-        } else if (*(key_ptr) == 3 && clockMode == 1) {//decrease time when in set alarm mode
+        } else if (*(key_ptr) == 4 && clockMode == 1 && buttonPressed == 0) {//decrease time when in set alarm mode
 
+			buttonPressed = 1;
             if (alarmSetSegment == 1) {//adjust seconds time
                 alarm_time_s--;
                 if (alarm_time_s == -1) {
@@ -152,8 +169,6 @@ int main(void) {
                 if (alarm_time_10s == -1) {
                     alarm_time_10s = 5;
                 }
-                DisplayTime(alarm_time_s, display_s);
-                DisplayTime(alarm_time_10s, display_10s);
             } else if (alarmSetSegment == 2) {//adjust minutes time
                 alarm_time_min--;
                 if (alarm_time_min == -1) {
@@ -163,8 +178,6 @@ int main(void) {
                 if (alarm_time_10min == -1) {
                     alarm_time_10min = 5;
                 }
-                DisplayTime(alarm_time_min, display_min);
-                DisplayTime(alarm_time_10min, display_10min);
             } else if (alarmSetSegment == 3) {//adjust hours time
                 alarm_time_hr--;
                 if (alarm_time_hr == -1) {
@@ -173,13 +186,19 @@ int main(void) {
                 }
                 if (alarm_time_10hr == -1) {
                     alarm_time_10hr = 2;
+					alarm_time_hr = 3;
                 }
-                DisplayTime(alarm_time_hr, display_hr);
-                DisplayTime(alarm_time_10hr, display_10hr);
             }
+            DisplayTime(alarm_time_s, display_s);
+            DisplayTime(alarm_time_10s, display_10s);
+            DisplayTime(alarm_time_min, display_min);
+            DisplayTime(alarm_time_10min, display_10min);
+            DisplayTime(alarm_time_hr, display_hr);
+            DisplayTime(alarm_time_10hr, display_10hr);
 
-        } else if (*(key_ptr) == 4 && clockMode == 1) {//increase time when in set alarm mode
+        } else if (*(key_ptr) == 8 && clockMode == 1 && buttonPressed == 0) {//increase time when in set alarm mode
 
+			buttonPressed = 1;
             if (alarmSetSegment == 1) {//adjust seconds time
                 alarm_time_s++;
                 if (alarm_time_s == 10) {
@@ -189,8 +208,6 @@ int main(void) {
                 if (alarm_time_10s == 6) {
                     alarm_time_10s = 0;
                 }
-                DisplayTime(alarm_time_s, display_s);
-                DisplayTime(alarm_time_10s, display_10s);
             } else if (alarmSetSegment == 2) {//adjust minutes time
                 alarm_time_min++;
                 if (alarm_time_min == 10) {
@@ -200,21 +217,27 @@ int main(void) {
                 if (alarm_time_10min == 6) {
                     alarm_time_10min = 0;
                 }
-                DisplayTime(alarm_time_min, display_min);
-                DisplayTime(alarm_time_10min, display_10min);
             } else if (alarmSetSegment == 3) {//adjust hours time
                 alarm_time_hr++;
-                if (alarm_time_hr == 10) {
+                if (alarm_time_hr == 10 || (alarm_time_hr == 4 && alarm_time_10hr == 2)) {
                     alarm_time_hr = 0;
                     alarm_time_10hr++;
                 }
                 if (alarm_time_10hr == 3) {
                     alarm_time_10hr = 0;
                 }
-                DisplayTime(alarm_time_hr, display_hr);
-                DisplayTime(alarm_time_10hr, display_10hr);
             }
+            DisplayTime(alarm_time_s, display_s);
+            DisplayTime(alarm_time_10s, display_10s);
+            DisplayTime(alarm_time_min, display_min);
+            DisplayTime(alarm_time_10min, display_10min);
+            DisplayTime(alarm_time_hr, display_hr);
+            DisplayTime(alarm_time_10hr, display_10hr);
         }
+		
+		if (*(key_ptr) != 1 && *(key_ptr) != 2 && *(key_ptr) != 4 && *(key_ptr) != 8) {
+			buttonPressed = 0;
+		}
     }
 }
 
